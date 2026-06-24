ea9f482223453dd1a8310cc756f34c74a5da0589
Pavel Romash <pavel_romash1@epam.com>
2026-06-24 13:45:54 +0400

    Added taking screenshoot feature

--- SeleniumFrameworkInteraction/Core/Base/BaseTest.cs
+++ SeleniumFrameworkInteraction/Core/Base/BaseTest.cs
@@ -1,9 +1,12 @@
+using Allure.Net.Commons;
 using Allure.NUnit;
 using Core.DI;
 using Core.Drivers;
 using Core.Enum;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
 
 namespace Core.Base;
 
@@ -31,6 +34,29 @@ public abstract class BaseTest
     [TearDown]
     public void QuitDriver()
     {
+        if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+        {
+            TryAttachScreenshot();
+        }
+
         ServiceLocator.GetService<IDriverManager>().Quit();
+    }
+
+    private void TryAttachScreenshot()
+    {
+        try
+        {
+            var driver = ServiceLocator.GetService<IDriverManager>().Current;
+            if (driver is ITakesScreenshot screenshotDriver)
+            {
+                var bytes = screenshotDriver.GetScreenshot().AsByteArray;
+                AllureApi.AddAttachment("Screenshot on Failure", "image/png", bytes, ".png");
+                Logger.LogInformation("[BaseTest] Screenshot attached to Allure report");
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogWarning(ex, "[BaseTest] Could not take screenshot on test failure");
+        }
     }
 }
