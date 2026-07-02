4c62625126371c57c445073e64f1205f85b005e4
Pavel Romash <pavel_romash1@epam.com>
2026-07-02 12:20:19 +0400

    Change jenkinsfile

--- vars/notifyTeams.groovy
+++ vars/notifyTeams.groovy
@@ -1,31 +1,25 @@
 def call(String status) {
 
-    def branch = sh(
-        script: 'git rev-parse --abbrev-ref HEAD',
-        returnStdout: true
-    ).trim()
+    def branch = env.BUILD_BRANCH
+        ?: env.GIT_BRANCH
+        ?: sh(script: 'git rev-parse --abbrev-ref HEAD', returnStdout: true).trim()
 
     def commit = sh(
         script: 'git rev-parse --short HEAD',
         returnStdout: true
     ).trim()
 
-    def author = sh(
-        script: 'git log -1 --pretty=format:"%an"',
-        returnStdout: true
-    ).trim()
+    def author = env.GIT_AUTHOR_NAME
+        ?: sh(script: 'git log -1 --pretty=format:"%an"', returnStdout: true).trim()
 
+    def message = """${status}
 
-    def message = """
-        ${status}
-
-        Job: ${env.JOB_NAME}
-        Build: #${env.BUILD_NUMBER}
-        Branch: ${branch}
-        Commit: ${commit}
-        Author: ${author}
-        ${env.BUILD_URL}
-    """.trim()
+Job: ${env.JOB_NAME}
+Build: #${env.BUILD_NUMBER}
+Branch: ${branch}
+Commit: ${commit}
+Author: ${author}
+${env.BUILD_URL}""".trim()
 
     echo """
 ================ Teams Notification ================
@@ -34,12 +28,12 @@ ${message}
 """
 
     withCredentials([string(credentialsId: 'TEAMS_WEBHOOK', variable: 'WEBHOOK')]) {
-        sh '''
-            curl -s -X POST "$WEBHOOK" \
-                 -H "Content-Type: application/json" \
-                 -d '{
-                       "text":"${message}"
-                     }'
-        '''
+        withEnv(["MSG=${message}"]) {
+            sh '''
+                curl -s -X POST "$WEBHOOK" \
+                     -H "Content-Type: application/json" \
+                     -d "{\\"text\\":\\"$MSG\\"}"
+            '''
+        }
     }
 }
