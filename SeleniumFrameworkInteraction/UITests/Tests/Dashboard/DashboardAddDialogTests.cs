4c8dd3afc471a715a2998a65ad9104b5ec93c93b
Pavel Romash <pavel_romash1@epam.com>
2026-07-06 18:28:33 +0400

    Change Jenkinsfile

--- SeleniumFrameworkInteraction/UITests/Tests/Dashboard/DashboardAddDialogTests.cs
+++ SeleniumFrameworkInteraction/UITests/Tests/Dashboard/DashboardAddDialogTests.cs
@@ -26,6 +26,7 @@ public class DashboardAddDialogTests : DashboardTestBase
 
     [Test]
     [Category("KAN-T1")]
+    [Property("testKey", "KAN-T1")]
     [Description("Cancel closes the Add New Dashboard dialog")]
     public void Cancel_ClosesDialog()
     {
